609eba4c391efdecb3d469f2089e54a81b8bf7ba
Jorge Sierra Acosta <jsierra.siete@gmail.com>
2018-05-14 09:27:34 +0100

    Updated cmake files
    Ready for windows

--- cmake/FindLibUSB.cmake
+++ cmake/FindLibUSB.cmake
@@ -1,22 +1,38 @@
-SET(LibUSB_DIR "C:/libusb-1.0.22")
+IF (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 
+  IF (NOT LIBUSB_ROOT_DIR)
+    SET(LIBUSB_ROOT_DIR "" CACHE PATH "Path to the libusb library root directory")
+    MESSAGE(FATAL_ERROR "LibUSB not found, please specify LIBUSB_ROOT_DIR")
+  ENDIF ()
 
+  SET(LIBUSB_INCLUDE_DIRS ${LIBUSB_ROOT_DIR}/include)
+  SET(LIBUSB_INCLUDES     ${LIBUSB_ROOT_DIR}/include)
+  SET(CMAKE_FIND_LIBRARY_PREFIXES "lib")
+  SET(CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".dll")
 
-SET(LibUSB_INCLUDE_DIRS ${LibUSB_DIR}/include)
-SET(LibUSB_INCLUDES     ${LibUSB_DIR}/include)
-SET(CMAKE_FIND_LIBRARY_PREFIXES "lib")
-SET(CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".dll")
-IF(MSVC OR ${CMAKE_GENERATOR} MATCHES "Xcode")
-  SET(LibUSB_LIBRARY_DIR  ${LibUSB_DIR}/MS32/dll)  # 32bits
+  # Library dir
+  IF(MSVC OR ${CMAKE_GENERATOR} MATCHES "Xcode")
+    SET(LIBUSB_LIBRARY_DIR  ${LIBUSB_ROOT_DIR}/MS32/dll)  # 32bits
+  ELSE ()
+    SET(LIBUSB_LIBRARY_DIR  ${LIBUSB_ROOT_DIR}/MinGW32/dll)  # 32bits
+  ENDIF ()
+
+  # Library
+  FIND_LIBRARY(LIBUSB_LIBS
+    NAMES usb-1.0 libusb usb libusb-1.0
+    PATHS ${LIBUSB_LIBRARY_DIR}
+    NO_DEFAULT_PATH
+    NO_CMAKE_ENVIRONMENT_PATH
+    NO_CMAKE_PATH
+    NO_SYSTEM_ENVIRONMENT_PATH
+    NO_CMAKE_SYSTEM_PATH
+    )
+
 ELSE ()
-  SET(LibUSB_LIBRARY_DIR  ${LibUSB_DIR}/MinGW32/dll)  # 32bits
+
+  # Copied from original repository https://github.com/maartenvds/libseek-thermal
+  SET(LIBUSB_INCLUDE_DIRS /usr/include/libusb-1.0)
+  SET(LIBUSB_INCLUDES     /usr/include/libusb-1.0)
+  SET(LIBUSB_LIBS -lusb-1.0)
+
 ENDIF ()
-FIND_LIBRARY(LibUSB_LIBS
-  NAMES usb-1.0 libusb usb libusb-1.0
-  PATHS ${LibUSB_LIBRARY_DIR}
-  NO_DEFAULT_PATH
-  NO_CMAKE_ENVIRONMENT_PATH
-  NO_CMAKE_PATH
-  NO_SYSTEM_ENVIRONMENT_PATH
-  NO_CMAKE_SYSTEM_PATH
-  )
